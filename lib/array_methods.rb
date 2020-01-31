40dbe35957ede97239fca3ea233a82361afd014a
Willis Aybar <willisaybar@gmail.com>
2020-01-31 01:49:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,17 +8,21 @@ def using_sort(array)
 end
 
 def using_reverse(array)
+  array = ["wow", "I"]
   array.reverse(array)
 end
 
 def using_first(array)
+  array = ["wow", "I"]
   array.first(array)
 end
 
 def using_last(array)
+  array = ["wow", "I"]
   array.last(array)
 end
 
 def using_size(array)
+  array = ["wow", "I"]
   array.size(array)
 end
