73cdfa08931bd43d4416afb7eda18398ceb89555
Willis Aybar <willisaybar@gmail.com>
2020-01-31 01:50:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,21 +8,21 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-  array = ["wow", "I"]
+  array = ["wow", "arrays!"]
   array.reverse
 end
 
 def using_first(array)
-  array = ["wow", "I"]
+  array = ["wow", "arrays!"]
   array.first
 end
 
 def using_last(array)
-  array = ["wow", "I"]
+  array = ["wow", "arrays!"]
   array.last
 end
 
 def using_size(array)
-  array = ["wow", "I"]
+  array = ["wow", "arrays!"]
   array.size
 end
